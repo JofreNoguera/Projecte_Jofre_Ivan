17c436e5f2c640260d1430c3666489dfebaf55de
JofreNoguera <152433700+JofreNoguera@users.noreply.github.com>
2024-02-22 15:39:56 +0100

    Add files via upload

---
+++ tk.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+ventana = tk.Tk()
+ventana.title("Projecte Python 2023-2024 | Jofre i Ivan")
+
+# Crear una etiqueta
+etiqueta = tk.Label(ventana, text="Prova")
+etiqueta.pack(padx=400, pady=300) # Añadir la etiqueta a la ventana
+# Iniciar el bucle de eventos
+
+ventana.mainloop()
